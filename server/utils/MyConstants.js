822208ec09ecb282a0e03c457c800f9db4c945d6
Hung2444 <sktt12403@gmail.com>
2023-11-23 14:43:52 +0700

    ShoppingOnline

---
+++ server/utils/MyConstants.js
@@ -0,0 +1,11 @@
+const MyConstants = {
+  DB_SERVER: 'cluster0.wc8lxrz.mongodb.net',
+  DB_USER: 'thinhtruong042',
+  DB_PASS: 'thinhtruong2009',
+  DB_DATABASE: 'shoppingonline',
+  JWT_SECRET: 'mihu',
+  JWT_EXPIRES: '36000000', // in milliseconds
+  EMAIL_USER: 'mihu2403@gmail.com', // gmail service
+  EMAIL_PASS: 'nsen bqgc jeoo gvid'
+};
+module.exports = MyConstants;
